c38ac0599a9bdf970b1f8f404290fc93234e33de
Gimmy <gimmys943@gmail.com>
2025-01-31 21:14:12 -0500

    fixed navbar, responsiveness fixes, and testimonial images

--- public/include.js
+++ public/include.js
@@ -1,28 +1,42 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const header = document.getElementById("header");
     const footer = document.getElementById("footer");
 
-    // Include header.html file
+    // Include navbar.html file
     fetch("./header.html")
-        .then(response => response.text())
-        .then(html => {
-            header.innerHTML = html;
-            
-            // Dropdown functionality for the menu button
-            const menuBtn = document.querySelector('.menu-btn');
-            const dropdown = document.querySelector('.dropdown');
-            
-            menuBtn.addEventListener('click', () => {
-                dropdown.classList.toggle('active');
-            });
-        })
-        .catch(error => console.error('Error loading header:', error));
+    .then((response) => response.text())
+    .then((html) => {
+        header.innerHTML = html;
 
-    // Including footer.html file on each page
-    fetch("./footer.html")
-        .then(response => response.text())
-        .then(html => {
-            footer.innerHTML = html;
-        })
-        .catch(error => console.error('Error loading footer:', error));
+        // Select the navbar after it is added to the DOM
+        const navbar = document.getElementById("nav");
+
+        // Dropdown functionality for the menu button
+        const menuBtn = document.querySelector(".menu-btn");
+        const dropdown = document.querySelector(".dropdown");
+
+        menuBtn.addEventListener("click", () => {
+            dropdown.classList.toggle("active");
+        });
+
+        // Add scroll event listener for the background color
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 0) {
+                navbar.classList.add("scrolled");
+                
+            } else {
+                navbar.classList.remove("scrolled");
+                
+            }
+        });
+    })
+    .catch((error) => console.error("Error loading navbar:", error));
+
+// Include footer.html file
+fetch("./footer.html")
+    .then((response) => response.text())
+    .then((html) => {
+        footer.innerHTML = html;
+    })
+    .catch((error) => console.error("Error loading footer:", error));
 });
